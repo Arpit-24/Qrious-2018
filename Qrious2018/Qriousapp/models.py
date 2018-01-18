4acc4c1efefd989983c6b283c652d49239bc2883
Aniruddha Karajgi <akarajgi0@gmail.com>
2018-01-18 22:29:16 +0530

    update models--2

--- Qrious2018/Qriousapp/models.py
+++ Qrious2018/Qriousapp/models.py
@@ -10,12 +10,6 @@ class CustUser(AbstractUser):
    num_diamonds = models.IntegerField(default=100)
 
 
-   # emerald stuff
-   """is_emer_red = models.BooleanField(default=False)
-            is_emer_blue = models.BooleanField(default=False)
-            is_emer_green = models.BooleanField(default=False)
-            is_emer_yellow = models.BooleanField(default=False)"""
-   
    num_emers = models.IntegerField(default=0)
    # score = # formula
 
@@ -34,9 +28,8 @@ class Level(models.Model):
 
 class Problem(models.Model):
    level = models.ForeignKey(Level, on_delete=models.CASCADE)
-   prob_num = models.IntegerField(blank=True, default=None)
    answer = models.CharField(max_length=1000)
-   # problem_media_____ question, images, the like...
+   prob_type = models.CharField(max_length=1000)
    prob_num_diam = models.IntegerField(default=50)
    prob_hint = models.CharField(max_length=1000)
    prob_help = models.CharField(max_length=1000)
